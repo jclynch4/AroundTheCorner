ae4812dc2bf3ffa45391b533089a38d5c352165c
Jacob Lynch <jacob.c.lynch@gmail.com>
2021-02-11 22:27:38 -0700

    changes to create account and script

--- AroundTheCorner/script.js
+++ AroundTheCorner/script.js
@@ -1,26 +1,34 @@
-var mysql = require('mysql');  
-var con = mysql.createConnection({  
-host: "107.180.1.16:3306",  
-user: "group62021",  
-password: "2021group6",  
-database: "group62021"  
-});  
-con.connect(function(err) {  
-if (err) throw err;  
-console.log("Connected!");  
-buyerid = getElementById("idBuyers").innerhtml
-name = getElementById("name").innerhtml
-age = getElementById("age").innerhtml
-email = getElementById("email").innerhtml
-username = getElementById("username").innerhtml
-password = getElementById("password").innerhtml
-location = getElementById("location").innerhtml
-admin = getElementById("admin").innerhtml
+window.onload = function() {
+	document.getElementById("createAccount").onclick = function createAccount() {
+		console.log("hi mom");
 
+		let name = document.getElementById("name").innerHTML
+		let age = document.getElementById("age").innerHTML
+		let email = document.getElementById("email").innerHTML
+		let username = document.getElementById("username").innerHTML
+		let password = document.getElementById("password").innerHTML
+		let location = document.getElementById("location").innerHTML
+		let admin = document.getElementById("admin").innerHTML
 
+		let mysql = require('mysql');  
+		console.log("hi mom");
+		var con = mysql.createConnection({  
+		host: "107.180.1.16",
+		port: "3306",  
+		user: "group62021",  
+		password: "2021group6",  
+		database: "group62021"  
+		});  
 
-var sql = "INSERT INTO Buyers (idBuyers, name, age, email, username, password, location, payment, admin) VALUES (buyerid, name, age, email, username, password, location, admin)";  
-con.query(sql, function (err, result) {  
-if (err) throw err;  
-console.log("1 record inserted");  
-});  
+		con.connect(function(err) {  
+		if (err) throw err;  
+		console.log("Connected!");
+
+		let sql = "INSERT INTO Buyers (name, age, email, username, password, location, payment, admin) VALUES ('name', '1', 'email', 'username', 'password', 'location', 'True')";  
+		con.query(sql, function (err, result) {  
+			if (err) throw err;  
+			console.log("1 record inserted");  
+		});  
+		});
+		}
+}
